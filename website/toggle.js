dae0368076b7e84d0cb31d376a86931f745687e4
Kamil Radzymiński <radzyminski.kamil@gmail.com>
2021-01-21 14:09:51 +0100

    Project Structure change

--- website/toggle.js
+++
@@ -1,9 +0,0 @@
-jQuery(function( $ ){
-
-    $('.faq-list .question').click( function(){
-		$(this).parent().find('.answer').slideToggle('fast');
-		$(this).toggleClass('on');
-		return false;
-	});
-
-});
